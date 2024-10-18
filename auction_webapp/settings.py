903d6680d907c374af42cfda36b23e11ad3c2ff8
PapaMencina <chicken914367204@gmail.com>
2024-10-18 15:47:44 -0700

    requirements.txt

--- auction_webapp/settings.py
+++ auction_webapp/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from dj_db_conn_pool import setup_connection_pool
 import redis
 import django_redis
 from dotenv import load_dotenv
